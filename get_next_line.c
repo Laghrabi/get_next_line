4979dd07f5a4f69a0884cdd7ebed3ccf48dc2df1
Chaimaa Laghrabi <claghrab@e1r8p5.1337.ma>
2024-11-22 00:07:01 +0100

    Backup

--- get_next_line.c
+++ get_next_line.c
@@ -1,211 +1,178 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: claghrab <claghrab@student.1337.ma>        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/18 16:31:20 by claghrab          #+#    #+#             */
-/*   Updated: 2024/11/18 16:31:24 by claghrab         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
+char *leftover(char *str, int nl)
 {
-	char *buffer;
-	static char *dup;
-	char *line;
-	size_t nl;
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
-	if (dup)
-	{
-		nl = tonl(dup, '\n');
-		if (nl)
-			return (ft_substr(dup, 0, len(dup, '\n')));
-	}
-	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (!buffer)
-		return (NULL);
-	nl = 0;
-	while (!nl)
-	{
-		ssize_t i = read(fd, buffer, BUFFER_SIZE);
-		if (i == 0)
-			return (NULL);
-		nl = tonl(buffer, '\n');
-		if (nl)
-		{
-			if (buffer + nl + 1 != '\0')
-			{
-				line = ft_strjoin(dup, ft_substr(buffer, 0, len(buffer, '\n')));
-				dup = ft_substr(buffer, nl + 1, BUFFER_SIZE - nl - 1);
-				return (line);
-			}
-			else
-				return (ft_strjoin(dup, buffer));
-		}
-		dup = ft_strjoin(dup, buffer);
-	}
-	return (NULL);
+    int i;
+    char *new;
+
+    if (!str)
+        return (NULL);
+    i = 0;
+    while (str[nl + i])
+        i++;
+    new = malloc(sizeof(char) * (i + 1));
+    if (!new)
+        return (NULL);
+    i = 0;
+    while (str[nl + i])
+    {
+        new[i] = str[nl + i];
+        i++;
+    }
+    return (new);
+}
+char *append_to_nl(char *str, char **line)
+{
+    int i;
+    char *new;
+
+    if (!str)
+        return (NULL);
+    i = 0;
+    while (str[i] && str[i] != '\n')
+        i++;
+    new = malloc(sizeof(char) * (i + 1));
+    if (!new)
+        return (NULL);
+    i = 0;
+    while (str[i] && str[i] != '\n')
+    {
+        new[i] = str[i];
+        i++;
+    }
+    return (new);
 }
 
 
-
-size_t	tonl(char *s, char c)
+char *dup(char *str)
 {
-	size_t	i;
+    size_t i;
+    char *new;
 
-	if (!s)
-		return (0);
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] == c)
-			return (i);
-		i++;
-	}
-	return (0);
+    if (!str)
+        return (NULL);
+    new = malloc(sizeof(char) * (ft_strlen(str) + 1));
+    if (!new)
+        return (NULL);
+    i = 0;
+    while (str[i])
+    {
+        new[i] = str[i];
+        i++;
+    }
+    new[i] = '\0';
+    return (new);
 }
 
-size_t	nl_func(char *s)
-{
-	size_t	i;
-
-	if (!s)
-		return (0);
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] == '\0')
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-char	*ft_substr(const char *s, unsigned int start, size_t len)
-{
-	size_t	sub_len;
-	size_t	i;
-	char	*sub;
-
-	if (!s)
-		return (NULL);
-	if (start >= ft_strlen(s))
-		return (NULL);
-	sub_len = ft_strlen(s + start);
-	if (len > sub_len)
-		len = sub_len;
-	sub = malloc(sizeof(char) * (len + 1));
-	if (!sub)
-		return (NULL);
-	i = 0;
-	while (i < len && s[start + i])
-	{
-		sub[i] = s[start + i];
-		i++;
-	}
-	sub[i] = '\0';
-	free(s);
-	return (sub);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*join(char *s1, char *s2, size_t len)
 {
 	size_t	i;
 	size_t	j;
 	char	*new;
 
 	if (!s1)
-		return (ft_strdup(s2));
+		return (dup(s2));
 	if (!s2)
-		return (ft_strdup(s1));
+		return (dup(s1));
 	new = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!new)
-		return (NULL);
+    {
+        free(s1);
+        return (NULL);
+    }
 	i = 0;
 	j = 0;
 	while (s1[i])
 		new[j++] = s1[i++];
 	i = 0;
-	while (s2[i])
+	while (s2[i] && i < len)
 		new[j++] = s2[i++];
 	new[j] = 0;
-	free(s1);
+    free(s1);
 	return (new);
 }
 
-char	*ft_strdup(const char *str)
+int tonl(char *s, char c)
 {
-	size_t	i;
-	char	*new;
+    int i;
 
-	new = malloc(sizeof(char) * ft_strlen(str) + 1);
-	if (!new)
-		return (NULL);
-	i = 0;
-	while (str[i])
-	{
-		new[i] = str[i];
-		i++;
-	}
-	new[i] = '\0';
-	return (new);
+    if (!s)
+        return (-1);
+    i = 0;
+    while (s[i])
+    {
+        if (s[i] == c)
+            return (i);
+        i++;
+    }
+    return (-1);
 }
 
-size_t	len(char *str, char c)
+char	*get_next_line(int fd)
 {
-	size_t	i;
+    static char	*last;
+    char *buffer;
+    char *line;
+    ssize_t i;
+    int nl;
 
-	i = 0;
-	while (str[i] && str[i] != c)
-		i++;
-	return (i);
-}
-size_t	ft_strlen(const char *str)
-{
-	size_t	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
-int	main(void)
-{
-	int		fd;
-	char	*buffer;
-
-	fd = open("test.txt", O_RDONLY);
-	if (fd == -1)
-	{
-		printf("open failed\n");
-		return (0);
-	}
-	while ((buffer = get_next_line(fd)) != 0)
-	{
-		printf("%s\n", buffer);
-		free(buffer);
-	}
-	// if (!buffer)
-	// {
-	//     printf("allocation failed\n");
-	//     return (0);
-	// }
-	// ssize_t read_value = read(fd, buffer, BUFFER_SIZE);
-	// if (read_value > 0)
-	//     printf("%s\n", buffer);
-	// else if (read_value == 0)
-	// {
-	//     printf("EOF\n");
-	//     printf("%d\n", EOF);
-	// }
-	// else
-	//     printf("error reading file");
-	// free(buffer);
-	close(fd);
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return (NULL);
+    buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+    if (!buffer)
+        return (NULL);
+    if (last)
+    {
+        line = dup(last);
+        free(last);
+    }
+    nl = -1;
+    while (nl == -1)
+    {
+        i = read(fd, buffer, BUFFER_SIZE);
+        if (i == -1 || i == 0)
+        {
+            free(buffer);
+            return (NULL);
+        }
+        buffer[i] = '\0';
+        nl = tonl(buffer, '\n');
+        if (nl != -1)
+        {
+            line = malloc(sizeof(char) * (nl + 1));
+            if (!line)
+            {
+                free(buffer);
+                return (NULL);
+            }
+            last = malloc(sizeof(char) * (i - nl));
+            if (!last)
+            {
+                free(buffer);
+                free(line);
+                return (NULL);
+            }
+            if(buffer[nl + 1] != '\0')
+            {
+                if (last)
+                    last = join(last, buffer + nl + 1, ft_strlen(buffer + nl + 1));
+                else
+                    last = dup(buffer + nl + 1);
+                line = join(line, buffer, nl + 1);
+                free(buffer);
+                return (line);
+            }
+            if (buffer[nl + 1] == '\0')
+            {
+                line = join(line, buffer, ft_strlen(buffer));
+                free(buffer);
+                return (line);
+            }
+        }
+        if (nl == -1)
+        {
+            line = dup(buffer);
+            free(buffer);
+        }
+    }
 }
--- get_next_line.h
+++ get_next_line.h
@@ -13,7 +13,6 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#define BUFFER_SIZE 4
 
 #include <stdlib.h>
 #include <unistd.h>
@@ -21,18 +20,22 @@
 #include <stdint.h>
 #include <stdio.h>
 
-char    *get_next_line1(int fd);
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+char    *get_next_line(int fd);
 //size_t	nl_func(char *s);
 int	tonl(char *s, char c);
 char	*ft_substr(char *s, unsigned int start, size_t len);
 char	*ft_strjoin(char *s1, char *s2);
-char	*ft_strdup(const char *str);
+char	*ft_strdup(char *str);
 size_t	len(char *str, char c);
 size_t	ft_strlen(const char *str);
 
 typedef struct t_buff
 {
-    char    buffer[1024];
+    char    buffer[3024];
     int     idx;
 } t_buff;
 
